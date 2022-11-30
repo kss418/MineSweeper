fbaa0f2921dd82e445b5f78c2f7e68cec7a58604
kss418 <jis1595@naver.com>
2022-11-30 13:21:13 +0900

    함수 작성

--- My project/Assets/Create_Box.cs
+++ My project/Assets/Create_Box.cs
@@ -6,12 +6,12 @@ public class Create_Box : MonoBehaviour
 {
     public GameObject Box;
     public float x = 15, y = 8, Box_size = 0.2f;
-
+    
     void Start()
     {
-        for(float i = -x; i <= x; i += Box_size)
+        for(float i = -x * Box_size ; i <= x * Box_size; i += Box_size)
         {
-            for(float j= -y;j <= y; j += Box_size)
+            for(float j= -y * Box_size;j <= y * Box_size; j += Box_size)
             {
                 Instantiate(Box, new Vector2(i, j), Quaternion.identity);
             }
--- My project/Assets/Create_Comp.cs
+++ My project/Assets/Create_Comp.cs
@@ -6,24 +6,50 @@ public class Create_Comp : MonoBehaviour
 {
     public GameObject Bomb, Num;
     public int Bomb_Count = 99;
-
-    void Create_Bomb(float x, float y, float Box_size)
+    public int[,] arr = new arr[16,30];
+    void Create_Bomb(float x, float y)
     {
         while (Bomb_Count > 0)
         {
-            for(float i = -x; i <= x; i += Box_size)
+            for(float i = 0; i < 2 * x; i++)
             {
-                for(float j = -y; j <= y; j += Box_size)
+                for(float j = 0; j < 2 * y; j++)
                 {
-                    Bomb_Count--;
+                    if (Bomb_Count == 0)
+                    {
+                        break;
+                    }
+
+                    int cur = Random.Range(0,6);
+                    if(cur == 0)
+                    {
+                        arr[i, j] = 1;
+                        Bomb_Count--;
+                    }
                 }
             }
         }
     }
 
-    void Create_Num()
+    void Create_Num(float x, float y)
     {
+        for(int i = 0; i < 2 * x; i++)
+        {
+            for(int j = 0; j < 2 * y; j++)
+            {
+                for(int l = -1;l <= 1; l++)
+                {
+                    for (int k = -1; k <= 1; k++)
+                    {
+                        if(l == 0 && k == 0)
+                        {
+                            continue;
+                        }
 
+                    }
+                }
+            }
+        }
     }
 
     void Start()
@@ -33,6 +59,6 @@ public class Create_Comp : MonoBehaviour
         float Box_size = GameObject.Find("Box_Generater").GetComponent<Create_Box>().Box_size;
 
         Create_Bomb(x, y, Box_size);
-        Create_Num();
+        Create_Num(x, y);
     }
 }
