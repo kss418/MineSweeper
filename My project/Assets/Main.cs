553fd8c00238c8899a0a9186cd122e4f48fb3b97
kss418 <jis1595@naver.com>
2022-12-01 12:59:31 +0900

    깃발, 폭탄 개수 표시 틀 추가

--- My project/Assets/Main.cs
+++ My project/Assets/Main.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Main : MonoBehaviour
 {
     Camera Cam;
     Vector2 MousePosition;
     float MaxDistance = 15f;
+    Text Bomb_Display;
+    int Bomb_Count = 99;
 
     void Delete()
     {
@@ -23,14 +26,37 @@ public class Main : MonoBehaviour
         }
     }
 
+    void Display_Bomb_Count()
+    {
+        Bomb_Display.text = Bomb_Count.ToString();
+    }
+
+    void Flag()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            MousePosition = Input.mousePosition;
+            MousePosition = Cam.ScreenToWorldPoint(MousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(MousePosition, transform.forward, MaxDistance);
+
+            if (hit)
+            {
+
+            }
+        }
+    }
+
     void Start()
     {
+        Bomb_Display = GameObject.Find("Bomb_Display").GetComponent<Text>();
         Cam = GetComponent<Camera>();
     }
 
 
     void Update()
     {
+        Display_Bomb_Count();
         Delete();
+        Flag();
     }
 }
