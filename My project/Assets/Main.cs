985495633ef81278e568dcec1104286045897b35
kss418 <jis1595@naver.com>
2022-12-06 14:24:12 +0900

    숫자 눌렀을 때 박스 제거

--- My project/Assets/Main.cs
+++ My project/Assets/Main.cs
@@ -12,7 +12,7 @@ public class Main : MonoBehaviour
     int Bomb_Count = 99;
     int alive = 1;
     public GameObject flag;
-    RaycastHit2D[] hits;
+    RaycastHit2D[] hits, hits2;
 
     void Delete()
     {
@@ -21,10 +21,11 @@ public class Main : MonoBehaviour
             MousePosition = Input.mousePosition;
             MousePosition = Cam.ScreenToWorldPoint(MousePosition);
             hits = Physics2D.RaycastAll(MousePosition, transform.forward, MaxDistance);
+            int Box_Check = 0;
 
-            for(int i = 0; i < hits.Length; i++)
+            for(int k = 0; k < hits.Length; k++)
             {
-                RaycastHit2D hit = hits[i];
+                RaycastHit2D hit = hits[k];
                 if(hit.collider.name == "Flag(Clone)")
                 {
                     break;
@@ -33,11 +34,58 @@ public class Main : MonoBehaviour
                 else if (hit.collider.name == "Box(Clone)")
                 {
                     Destroy(hit.collider.gameObject);
+                    Box_Check = 1;
+                    if(hits.Length == 1)
+                    {
+
+                    }
                 }
 
                 else if (hit.collider.name == "Bomb(Clone)")
                 {
                     alive = 0;
+                }
+
+                else if (hit.collider.name == "Num(Clone)" && Box_Check == 0)
+                {
+                    int cnt = 0;
+                    for (float i = -0.2f; i <= 0.2f; i += 0.2f)
+                    {
+                        for (float j = -0.2f; j <= 0.2f; j += 0.2f)
+                        {
+                            RaycastHit2D curhit = Physics2D.Raycast(MousePosition, transform.forward, MaxDistance);
+                            if(curhit.collider.name == "Flag(Clone)")
+                            {
+                                cnt++;
+                            }
+                        }
+                    }
+
+                    for (float i = -0.2f;i <= 0.2f; i += 0.2f)
+                    {
+                        for (float j = -0.2f; j <= 0.2f; j += 0.2f)
+                        {
+                            hits2 = Physics2D.RaycastAll(new Vector2(MousePosition.x + i,MousePosition.y + j), transform.forward, MaxDistance);
+                            for (int l = 0; l < hits2.Length; l++)
+                            {
+                                RaycastHit2D hit2 = hits2[l];
+                                if (hit2.collider.name == "Flag(Clone)")
+                                {
+                                    break;
+                                }
+
+                                else if (hit2.collider.name == "Box(Clone)")
+                                {
+                                    Destroy(hit2.collider.gameObject);
+                                }
+
+                                else if (hit2.collider.name == "Bomb(Clone)")
+                                {
+                                    alive = 0;
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -67,8 +115,8 @@ public class Main : MonoBehaviour
                 {
                     Bomb_Count--;
                     float Box_size = GameObject.Find("Box_Generater").GetComponent<Create_Box>().Box_size;
-                    float x = (int)(5 * (MousePosition.x - 0.1f));
-                    float y = (int)(5 * (MousePosition.y - 0.1f));
+                    float x = (int)(5 * (MousePosition.x));
+                    float y = (int)(5 * (MousePosition.y));
                     x *= Box_size;
                     y *= Box_size;
                     Instantiate(flag, new Vector3(x, y, -1), Quaternion.identity);
